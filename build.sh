4dd1c34759fc22639f169b1d07d054653141f033
Nicolas Dufreche <nicolas.dufreche@lp-digital.fr>
2015-03-31 14:05:21 +0200

    testing

--- build.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-echo "Vendor dir creation"
-mkdir dist/vendor
-
-echo "Moving dependencies into vendor dir"
-cp bower_components/* dist/vendor/*
-cp node_modules/jsclass dist/vendor/jsclass
